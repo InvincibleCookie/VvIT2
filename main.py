600aedc20bf7efebbfcfd62e71823da47865ccf3
InvincibleCookie <stav2004@mail.ru>
2023-02-13 13:42:02 +0300

    Погода в Красноярске на неделю

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+city = "Krasnoyarsk,RU"
+appid = "c46564d10a1f5fd5a404bb74b58ee294"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
+    print("____________________________")
